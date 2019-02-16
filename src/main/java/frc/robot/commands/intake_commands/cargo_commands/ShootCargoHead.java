9b39351aa5a6d8e61e5de3be1f065200178a97e7
unknown <team1024programming@gmail.com>
2019-02-16 15:18:08 -0500

    Added in end statement

--- src/main/java/frc/robot/commands/intake_commands/cargo_commands/ShootCargoHead.java
+++ src/main/java/frc/robot/commands/intake_commands/cargo_commands/ShootCargoHead.java
@@ -41,6 +41,7 @@ public class ShootCargoHead extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.intake.cargoheadStop();
   }
 
   // Called when another command which requires one or more of the same
